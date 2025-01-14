aa98a87f4216a68068781f99cb8da402d9f4557c
polaris65b <42924432+polaris65b@users.noreply.github.com>
2025-01-14 21:30:58 +0900

    test
    test

--- KioskProject/src/Lv1/Kiosk.java
+++
@@ -1,6 +0,0 @@
-package Lv1;
-
-// Kiosk : 프로그램 순서 및 흐름 제어를 담당하는 클래스
-
-public class Kiosk {
-}
--- KioskProject/src/Lv1/MenuItem.java
+++
@@ -1,9 +0,0 @@
-package Lv1;
-
-//MenuItem : 세부 속성 가지는 클래스
-//햄버거의 이름, 가격 설명
-//    예시 : Shackburger, 6.9, 토마토, 양상추, 쉑소스가 토핑된 치즈버거
-//    new MenuItem("sheackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거")
-
-public class MenuItem {
-}
