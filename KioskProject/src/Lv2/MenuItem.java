ccb327d9847f96540549630ddc6080404e949ea3
polaris65b <42924432+polaris65b@users.noreply.github.com>
2025-01-14 21:47:52 +0900

    Lv2 필드 생성

---
+++ KioskProject/src/Lv2/MenuItem.java
@@ -0,0 +1,12 @@
+package Lv2;
+
+// 개별 음식 항목을 관리하는 클래스입니다. 현재는 햄버거만 관리합니다.
+// 클래스는 이름, 가격, 설명 필드를 갖습니다.
+public class MenuItem {
+    // 필드
+    private String name;            //  이름
+    private int price;              //  가격
+    private String description;     //  설명
+
+
+}
