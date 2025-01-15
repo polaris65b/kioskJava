766c00b3256a2bf6c925a82a4baacb6c470e8a8d
polaris65b <42924432+polaris65b@users.noreply.github.com>
2025-01-15 20:46:23 +0900

    Lv4세팅

---
+++ KioskProject/src/Lv4/Kiosk.java
@@ -0,0 +1,23 @@
+package Lv4;
+
+public class Kiosk {
+
+    public void start() {
+        // 스캐너 선언
+        // 반복문 시작
+
+        // List와 Menu 클래스 활용하여 상위 카테고리 메뉴 출력
+
+        // 숫자 입력 받기
+
+        // 입력 받은 숫자가 올바르다면 인덱스로 활용하여 List에 접근하기
+        // List<Menu>에 인덱스로 접근하면 Menu만 추출할 수 있겠죠?
+
+        // Menu가 가진 List<MenuItem>을 반복문을 활용하여 햄버거 메뉴 출력
+
+        // 숫자 입력 받기
+        // 입력 받은 숫자가 올바르다면 인덱스로 활용해서 Menu가 가지고 있는 List<MenuItem>에 접근하기
+        // menu.getMenuItems().get(i); 같은 형식으로 하나씩 들어가서 얻어와야 합니다.
+    }
+
+}
