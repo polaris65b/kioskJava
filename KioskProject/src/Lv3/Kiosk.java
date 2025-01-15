d434ee053cb9ac1140280773d5f7ebd6c0f7a0a5
polaris65b <42924432+polaris65b@users.noreply.github.com>
2025-01-15 16:58:41 +0900

    피드백 이후 수정
    `List<MenuItem> menuItems` 는 `Kiosk` 클래스 생성자를 통해 값을 할당합니다.
    
    `Kiosk` 객체를 생성하고 사용하는 `main` 함수에서 객체를 생성할 때 값을 넘겨줍니다.

--- KioskProject/src/Lv3/Kiosk.java
+++ KioskProject/src/Lv3/Kiosk.java
@@ -12,23 +12,17 @@ import java.util.Scanner;
 
 public class Kiosk {
     // 필드
-    private final List<MenuItem> menuItems = new ArrayList<>();
+    private List<MenuItem> menuItems = new ArrayList<>();
+
+    // 생성자
+    public Kiosk(List<MenuItem> menuItems){
+        this.menuItems = menuItems;
+    }
 
     // 메서드
-    // 메뉴를 저장하는 메서드
-    public void addMenu(){
-        menuItems.add(new MenuItem("ShackBurger", 6.9,
-                "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("SmokeShack", 8.9,
-                "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("Cheeseburger", 6.9,
-                "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("Hamburger", 5.4,
-                "비프패티를 기반으로 야채가 들어간 기본버거"));
-    }
     // Kiosk가 동작하는 방법을 정의한 메서드
     public void start(){
-        addMenu();  //  메뉴를 추가함.
+        // addMenu();  //  메뉴를 추가함.
         Scanner sc = new Scanner(System.in);
 
         while (true) {
