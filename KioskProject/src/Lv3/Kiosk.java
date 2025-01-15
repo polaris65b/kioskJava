1da90696dc8ecc5d338efcc42caebfb0c16a3cb7
polaris65b <42924432+polaris65b@users.noreply.github.com>
2025-01-15 15:37:09 +0900

    Lv3 추가 수정
    1. addMenu 생성 :
    2. 주석 추가
    3. 예외처리
    1) int choiceMenu에서 정수 이외의 값이 들어올 때
    1 - 1) 정수 이외의 값이 들어올 때 무한 루프가 되는 것을 발견, sc.nextLine()을 추가함으로써 해결

--- KioskProject/src/Lv3/Kiosk.java
+++ KioskProject/src/Lv3/Kiosk.java
@@ -15,7 +15,8 @@ public class Kiosk {
     private final List<MenuItem> menuItems = new ArrayList<>();
 
     // 메서드
-    public void start(){
+    // 메뉴를 저장하는 메서드
+    public void addMenu(){
         menuItems.add(new MenuItem("ShackBurger", 6.9,
                 "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
         menuItems.add(new MenuItem("SmokeShack", 8.9,
@@ -24,33 +25,43 @@ public class Kiosk {
                 "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
         menuItems.add(new MenuItem("Hamburger", 5.4,
                 "비프패티를 기반으로 야채가 들어간 기본버거"));
-
+    }
+    // Kiosk가 동작하는 방법을 정의한 메서드
+    public void start(){
+        addMenu();  //  메뉴를 추가함.
         Scanner sc = new Scanner(System.in);
 
         while (true) {
-            System.out.println("[ SHAKESHACK MENU ]");
-            int index = 1;
-            for (MenuItem item : menuItems) {
-                System.out.println(index + ". " + item.getName()
-                        + "   | W " + item.getPrice()
-                        + " | " + item.getDescription());
-                index++;
+            try{
+                // 메뉴 출력
+                System.out.println("       [ SHAKESHACK MENU ]");
+                int index = 1;
+                for (MenuItem item : menuItems) {
+                    System.out.println(index + ". " + item.getName()
+                            + "   | W " + item.getPrice()
+                            + " | " + item.getDescription());
+                    index++;
+                }
+                System.out.println("0. 종료       |       종료");
+                // 번호 입력 받음
+                System.out.print("번호 입력: ");
+                int choiceMenu = sc.nextInt();
+                if (choiceMenu == 0) {
+                    System.out.println("프로그램을 종료합니다.");
+                    break;
+                } else if (choiceMenu > 0 && choiceMenu <= menuItems.size()) {
+                    MenuItem selectedItem = menuItems.get(choiceMenu - 1);
+                    System.out.println("선택한 메뉴: " + selectedItem.getName()
+                            + ", W " + selectedItem.getPrice()
+                            + ", " + selectedItem.getDescription());
+                } else {
+                    System.out.println("잘못된 입력입니다. 다시 입력하세요.");
+                }
             }
-            System.out.println("0. 종료       |       종료");
-
-            System.out.print("번호 입력: ");
-            int choiceMenu = sc.nextInt();
-
-            if (choiceMenu == 0) {
-                System.out.println("프로그램을 종료합니다.");
-                break;
-            } else if (choiceMenu > 0 && choiceMenu <= menuItems.size()) {
-                MenuItem selectedItem = menuItems.get(choiceMenu - 1);
-                System.out.println("선택한 메뉴: " + selectedItem.getName()
-                        + ", W " + selectedItem.getPrice()
-                        + ", " + selectedItem.getDescription());
-            } else {
-                System.out.println("잘못된 입력입니다. 다시 입력하세요.");
+            // int choiceMenu에서 정수가 아닌 수가 들어오는 경우 예외처리
+            catch (Exception e) {
+                System.out.println("숫자를 입력해주세요");
+                sc.nextLine(); // 입력 버퍼에 남아있어 무한 루프 되는 것을 방지
             }
         }
     }
